7173859e4268c84c6706fdcbb319c710ff9fe806
Rayllan Santos de Souza <rayllan51@gmail.com>
2019-07-07 00:22:23 -0300

    Delete

--- resources/header.php
+++ resources/header.php
@@ -1,1 +1,13 @@
+<html>
+    <head>
+	  <title>Controle de Produtos</title>
+      <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+	  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css"> 
 
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+<body>
