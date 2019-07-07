5b5f173b99713fdeca22a09c7c171eef19bfc063
Rayllan Santos de Souza <rayllan51@gmail.com>
2019-07-07 14:15:56 -0300

    Create net.bat

---
+++ net.bat
@@ -0,0 +1,759 @@
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
+ipconfig/release
+ipconfig/renew
+Timeout /t 60
