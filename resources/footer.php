0a4459a8ffe02dcf47c470fd3085ee8c43049a0d
Rayllan Santos de Souza <rayllan51@gmail.com>
2019-07-07 00:24:00 -0300

    Update footer.php

--- resources/footer.php
+++ resources/footer.php
@@ -1,1 +1,9 @@
-
+		<div class="col s12 orange">
+			<div class="container">
+			<h6 class = "noselect black-text center-align">© Marcela Todos os Direitos Reservados.</h6>
+			</div>
+		</div>
+      <!--JavaScript at end of body for optimized loading-->
+      <script type="text/javascript" src="js/materialize.min.js"></script>
+    </body>
+  </html>
