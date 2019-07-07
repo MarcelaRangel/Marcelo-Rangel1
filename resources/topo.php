d411abfd74341956d43686e7d9bde58529939aef
Rayllan Santos de Souza <rayllan51@gmail.com>
2019-07-07 13:40:26 -0300

    Update topo.php

--- resources/topo.php
+++ resources/topo.php
@@ -1,4 +1,5 @@
   <nav>
+  <div class="col-s8">
     <div class="nav-wrapper">
       <a href="#" class="brand-logo">Sistema de Controle</a>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
@@ -7,4 +8,5 @@
         <li><a href="removerproduto.php">Remover Produtos</a></li>
       </ul>
     </div>
+   </div>
   </nav>
