1d3d65119e3bbf18d8d774756166fb7c11ff4152
rayllan7029 <52614397+rayllan7029@users.noreply.github.com>
2019-07-16 01:07:30 -0300

    Update conexao.php

--- db/conexao.php
+++ db/conexao.php
@@ -1,5 +1,5 @@
 
 <?php
 $utf8=header("Content-Type: text/HTML; charset=utf-8");
-$link=new mysqli('localhost','root','','controle');
-$link->set_charset('utf8');
+$link=new mysqli('localhost','root','','controle');//Crio conexão nesta linha.
+$link->set_charset('utf8');//seto o char para nao ter problemas de identificação.
